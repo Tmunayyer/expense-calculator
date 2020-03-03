c7e597603759fdd4f46cafe8766210b025a436da
Thomas Munayyer <tmunayyer@gmail.com>
2020-03-02 21:22:36 -0500

    install babel, simple state management

--- public/bundle.js
+++ public/bundle.js
@@ -226,7 +226,7 @@ eval("\n\nif (false) {} else {\n  module.exports = __webpack_require__(/*! ./cjs
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"App\", function() { return App; });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\nconst App = () => {\n  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"div\", null, \"Hello World\");\n};\n\n//# sourceURL=webpack:///./src/app.jsx?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"App\", function() { return App; });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\nconst App = () => {\n  const [count, setCount] = Object(react__WEBPACK_IMPORTED_MODULE_0__[\"useState\"])(0);\n  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"div\", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"div\", null, `the count: ${count}`), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"button\", {\n    onClick: () => setCount(count + 1)\n  }, \"count\"));\n};\n\n//# sourceURL=webpack:///./src/app.jsx?");
 
 /***/ }),
 
--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const App = () => {
-  return <div>Hello World</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <div>{`the count: ${count}`}</div>
+      <button onClick={() => setCount(count + 1)}>count</button>
+    </div>
+  );
 };
