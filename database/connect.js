8fec7ea8fde775111f26308f41bc5b510b2b1a60
Thomas Munayyer <tmunayyer@gmail.com>
2020-03-04 20:46:12 -0500

    init.sql firing in production

--- database/connect.js
+++ database/connect.js
@@ -1,8 +1,33 @@
 // creates and exports the postgres pool using environment variables set in .env
 
 const { Pool } = require('pg');
+const fs = require('fs');
 
-const pool = new Pool();
+let pool;
+
+if (process.env.MODE === 'development') {
+  pool = new Pool();
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  });
+
+  const text = fs.readFileSync('./database/init.sql').toString();
+
+  pool.connect((err, client) => {
+    if (err) {
+      throw new Error(err);
+    }
+    client.query(text, (err, data) => {
+      if (err) {
+        throw new Error(err);
+      }
+
+      client.release();
+    });
+  });
+}
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE expense_calculator;
-
-CREATE TABLE "user" (
+CREATE TABLE IF NOT EXISTS "user" (
     id serial primary key,
     google_id text UNIQUE,
     full_name varchar(255),
@@ -9,7 +7,7 @@ CREATE TABLE "user" (
     avatar text
 );
 
-CREATE TABLE "calculator" (
+CREATE TABLE IF NOT EXISTS "calculator" (
     id serial primary key,
     user_id integer REFERENCES "user"(id) UNIQUE,
     election float default null,
